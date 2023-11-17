cdc4a3447758b1f051213ecd1296494c9752ac78
Michal Fousek <chlup@chlup.info>
2023-11-17 17:28:06 +0100

    Add empty project with Zcash SDK as dependency

---
+++ BlockchainMessenger/BlockchainMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockchainMessengerApp.swift
+//  BlockchainMessenger
+//
+//  Created by Michal Fousek on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BlockchainMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
