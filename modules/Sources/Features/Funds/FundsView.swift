a81c8e4bb99cae48a8f5aedda2e522a33e1e3ac8
Michal Fousek <chlup@chlup.info>
2023-11-30 13:45:11 +0100

    Add license text to FundsView

--- modules/Sources/Features/Funds/FundsView.swift
+++ modules/Sources/Features/Funds/FundsView.swift
@@ -42,28 +42,11 @@ public struct FundsView: View {
     
     public var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack(spacing: 20) {
-                Text("Address of your account")
-                
-                Text(viewStore.uAddress.data)
-                    .font(.system(size: 13))
-                    .foregroundStyle(Asset.Colors.fontPrimary.color)
-                    .padding()
-                    .background {
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundStyle(Asset.Colors.buttonBackground.color)
-                            .neumorphicShape(cornerRadius: 10)
-                    }
-                
-                Button {
-                    viewStore.send(.tapToCopyTapped(viewStore.uAddress))
-                } label: {
-                    Text("Tap to copy")
-                        .underline()
-                }
+            ScrollView(.vertical) {
+                VStack(spacing: 20) {
+                    Text("Address of your account")
 
-                if let sAddress = viewStore.sAddress {
-                    Text(sAddress.data)
+                    Text(viewStore.uAddress.data)
                         .font(.system(size: 13))
                         .foregroundStyle(Asset.Colors.fontPrimary.color)
                         .padding()
@@ -72,16 +55,63 @@ public struct FundsView: View {
                                 .foregroundStyle(Asset.Colors.buttonBackground.color)
                                 .neumorphicShape(cornerRadius: 10)
                         }
-                    
+
                     Button {
-                        viewStore.send(.tapToCopyTapped(sAddress))
+                        viewStore.send(.tapToCopyTapped(viewStore.uAddress))
                     } label: {
                         Text("Tap to copy")
                             .underline()
                     }
+                    
+                    if let sAddress = viewStore.sAddress {
+                        Text(sAddress.data)
+                            .font(.system(size: 13))
+                            .foregroundStyle(Asset.Colors.fontPrimary.color)
+                            .padding()
+                            .background {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundStyle(Asset.Colors.buttonBackground.color)
+                                    .neumorphicShape(cornerRadius: 10)
+                            }
+
+                        Button {
+                            viewStore.send(.tapToCopyTapped(sAddress))
+                        } label: {
+                            Text("Tap to copy")
+                                .underline()
+                        }
+                    }
+
+                    Spacer()
+
+                    Text("""
+                    LICENSE:
+
+                    MIT License
+
+                    Copyright (c) 2023 Mugeaters
+
+                    Permission is hereby granted, free of charge, to any person obtaining a copy
+                    of this software and associated documentation files (the "Software"), to deal
+                    in the Software without restriction, including without limitation the rights
+                    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                    copies of the Software, and to permit persons to whom the Software is
+                    furnished to do so, subject to the following conditions:
+
+                    The above copyright notice and this permission notice shall be included in all
+                    copies or substantial portions of the Software.
+
+                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                    SOFTWARE.
+                    """)
+
+                    Spacer()
                 }
-                
-                Spacer()
             }
             .padding()
             .onAppear {
